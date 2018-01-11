0ac53f2ab1611a5333d7acdd478678065ea337f5
rebeccabex <rebeccabexb@gmail.com>
2018-01-11 09:36:34 +0000

    More MouseActions tests

--- src/test/java/MouseActions/AccordionTest.java
+++ src/test/java/MouseActions/AccordionTest.java
@@ -1,4 +1,36 @@
 package MouseActions;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class AccordionTest {
+
+    private DemoQA demoQA;
+
+    @BeforeClass
+    public void setup() {
+
+        demoQA = new DemoQA("chrome");
+        demoQA.begin("accordion/");
+
+    }
+
+    @AfterClass
+    public void teardown() {
+        demoQA.end(3000);
+    }
+
+    @Test
+    public void testDefaultFunctionality() {
+
+        demoQA.clickById("ui-id-1");
+
+        for (int i = 10; i >= 4; i -= 2) {
+            demoQA.clickById("ui-id-" + i);
+            demoQA.wait(1000);
+        }
+
+    }
+
 }
--- src/test/java/MouseActions/SortableTest.java
+++ src/test/java/MouseActions/SortableTest.java
@@ -1,7 +1,6 @@
 package MouseActions;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -40,7 +39,7 @@ public class SortableTest {
         demoQA.wait(1000);
 
         actions.clickAndHold(demoQA.getDriver().findElement(By.cssSelector("#sortable > li:nth-child(6)"))).
-                moveByOffset(0, -30).release().build().perform();
+                moveByOffset(0, -100).release().build().perform();
 
         demoQA.wait(1000);
 
